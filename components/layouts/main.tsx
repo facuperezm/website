93ee11f61887eb2400f96c05e51bd8d3283154a6
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-04-12 16:04:05 -0300

    chore: fixed meta tags, and topblur component

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -9,29 +9,41 @@ const Main = ({ children }: Props) => {
   return (
     <>
       <Head>
-        <title>Facundo Perez Montalvo</title>
+        <title>Facundo Perez Montalvo - Frontend Developer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
-          content="I'm currently looking for a new role as a developer. Hire me?"
+          content="I'm a frontend developer with experience in React, Next.js, and Tailwind CSS. Currently seeking new opportunities."
         />
         <meta
           name="keywords"
-          content="Facundo Perez Montalvo, developer, react, next.js, tailwind css, frontend developer, frontend, software development, vercel"
+          content="Facundo Perez Montalvo, developer, react, next.js, tailwind css, frontend developer, software development, vercel"
         />
         <meta name="author" content="Facundo Perez Montalvo" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <!-- Twitter --> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@facuperezm" />
         <meta name="twitter:creator" content="@facuperezm" />
+        <meta
+          name="twitter:title"
+          content="Facundo Perez Montalvo - Frontend Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="I'm a frontend developer with experience in React, Next.js, and Tailwind CSS. Currently seeking new opportunities."
+        />
         <meta name="twitter:image" content="https://i.imgur.com/fjL49nC.jpg" />
-        <meta name="og:image" content="https://i.imgur.com/fjL49nC.jpg" />
+        {/* <!-- Open Graph --> */}
         <meta property="og:site_name" content="Facundo Perez Montalvo" />
-        <meta property="og:type" content="personal website" />
-        <meta property="og:title" content="Facundo Perez Montalvo" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Facundo Perez Montalvo - Frontend Developer"
+        />
         <meta
           property="og:description"
-          content="I'm currently looking for a new role as a developer. Hire me?"
+          content="I'm a frontend developer with experience in React, Next.js, and Tailwind CSS. Currently seeking new opportunities."
         />
         <meta property="og:url" content="https://facuperezm.vercel.app" />
         <meta property="og:image" content="https://i.imgur.com/fjL49nC.jpg" />
