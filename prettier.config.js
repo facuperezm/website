385aadfa10dde83eac0943b4d8a8fd4bc3db1b71
facuperezm <facundoperezmontalvo@gmail.com>
2023-11-24 15:17:09 -0300

    Update dependencies and configuration files

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tailwindAttributes: ["tw"],
+  tailwindFunctions: ["cva"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+};
