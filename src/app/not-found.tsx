5c936394abde196fbf1bae4a08a1d531d2b0ddbf
facuperezm <facundoperezmontalvo@gmail.com>
2023-11-24 16:50:17 -0300

    feat: Added not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { TrafficCone } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <div className="grid h-full grid-cols-1 place-content-center place-items-center">
+      <div className="h-full text-center">
+        <TrafficCone className="mx-auto mb-4 h-16 w-16 text-red-500" />
+        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Not found
+        </h2>
+        <p className="mb-4 leading-7">Could not find requested resource</p>
+        <Link className={buttonVariants({ variant: "outline" })} href="/">
+          Return Home
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/components/footer-component.tsx
+++ src/components/footer-component.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./ui/heart-theme";
 
 function Footer() {
   return (
-    <footer className="py-3 text-center text-sm text-muted-foreground/80">
+    <footer className="py-3 text-center text-sm text-muted-foreground">
       Built with <ModeToggle className="px-2" /> by{" "}
       <Link href={siteConfig.links.linkedin} className="hover:underline">
         {siteConfig.nickname}
