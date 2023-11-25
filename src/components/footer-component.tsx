8f89bf5deb2d72b01f33d7e4ddd79bca1bd24002
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-11-25 15:51:12 -0300

    Update footer component styles

--- src/components/footer-component.tsx
+++ src/components/footer-component.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./ui/heart-theme";
 
 function Footer() {
   return (
-    <footer className="container flex max-w-3xl flex-col justify-between gap-1 py-3 text-center text-sm text-muted-foreground md:flex-row">
+    <footer className="mx-auto flex max-w-3xl flex-col justify-between gap-1 px-4 py-3 text-center text-sm text-muted-foreground md:flex-row">
       <p>
         <Link href={siteConfig.links.repo} className="hover:underline">
           Built
