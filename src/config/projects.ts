ee6fa2736fe2316fdfe7537b4e55c33c32efa601
facuperezm <facundoperezmontalvo@gmail.com>
2024-01-30 18:28:45 -0300

    changed projects

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -7,6 +7,23 @@ export type Project = {
 
 export const PROJECTS = [
   {
+    title: "cartaonline.",
+    link: "https://github.com/facuperezm/frontend-cartaonline-next",
+    tech: [
+      "Next.js",
+      "Tailwind CSS",
+      "Vercel",
+      "Radix UI",
+      "Clerk",
+      "UploadThing",
+      "Zod",
+      "Prisma",
+      "MySQL",
+    ],
+    description:
+      "🍕 Cartaonline is an app that allows you to create and manage your own online menu.",
+  },
+  {
     title: "Basement. Challenge",
     description: "🏴‍☠️ This is my solution to the Basement Frontend Challenge.",
     link: "https://basement.facupm.dev",
@@ -20,13 +37,6 @@ export const PROJECTS = [
       "📚 The application is a HackerNews clone that highlights the top 10 stories using an <iframe/> component.",
   },
   {
-    title: "Password Generator",
-    link: "https://password.facupm.dev/",
-    tech: ["Next.js", "TypeScript", "Tailwind CSS", "Vercel"],
-    description:
-      "🔐 Password Generator is a simple app that generates a random password based on the user's input.",
-  },
-  {
     title: "URL Shortening",
     link: "https://url.facupm.dev",
     tech: ["Vite.js", "Tailwind CSS", "Vercel"],
@@ -34,10 +44,11 @@ export const PROJECTS = [
       "🔗 URL Shorting is a simple app that allows you to shorten URLs.",
   },
   {
-    title: "Loco Heraldo's Menu",
-    link: "https://locoheraldo.vercel.app/",
-    tech: ["Next.js", "Tailwind CSS", "Vercel", "i18n", "Google Sheets"],
-    description: "🍕 Freelance work for a local restaurant.",
+    title: "Password Generator",
+    link: "https://password.facupm.dev/",
+    tech: ["Next.js", "TypeScript", "Tailwind CSS", "Vercel"],
+    description:
+      "🔐 Password Generator is a simple app that generates a random password based on the user's input.",
   },
   {
     title: "e-Store",
@@ -46,23 +57,9 @@ export const PROJECTS = [
     description: "🛒 E-commerce store that uses the Fake Store API.",
   },
   {
-    title: "Advice App",
-    link: "https://advice.facupm.dev",
-    tech: ["Next.js", "Tailwind CSS", "Advice Slip API"],
-    description:
-      "🏃‍♂️ Advice Generator is a simple app that generates random advice.",
-  },
-  {
     title: "Image Uploader",
     link: "https://uploader.facupm.dev",
     tech: ["React", "Tailwind CSS", "Firebase"],
     description: "📷 Image uploader app with drag & drop area",
-  },
-  {
-    title: "Cartaonline [Work in progress...]",
-    link: "https://github.com/facuperezm/frontend-cartaonline-next",
-    tech: ["Next.js", "Tailwind CSS", "Vercel", "Radix UI", "Clerk"],
-    description:
-      "🍕 Cartaonline is a web app that allows you to create your own online menu.",
   },
 ] as Project[];
