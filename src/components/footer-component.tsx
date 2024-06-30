160664ae27a2df13417960314c1a8d7079042eb2
facuperezm <facundoperezmontalvo@gmail.com>
2024-06-30 19:41:38 -0300

    chore: Update max width of footer component to 4xl

--- src/components/footer-component.tsx
+++ src/components/footer-component.tsx
@@ -14,7 +14,7 @@ function Footer() {
 
   return (
     <>
-      <footer className="mx-auto flex max-w-3xl flex-col justify-between gap-1 px-4 py-3 text-center text-sm text-muted-foreground md:flex-row">
+      <footer className="mx-auto flex max-w-4xl flex-col justify-between gap-1 px-4 py-3 text-center text-sm text-muted-foreground md:flex-row">
         <p>
           <Link href={siteConfig.links.repo} className="hover:underline">
             Built
