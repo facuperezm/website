510e109db1e53ed208c8aa63449af33b3bff1b35
facuperezm <facundoperezmontalvo@gmail.com>
2025-06-07 09:23:22 -0300

    refactor: simplify About Me section markup and enhance styling for better readability; comment out introductory text

--- src/app/_components/about-me-component.tsx
+++ src/app/_components/about-me-component.tsx
@@ -11,26 +11,23 @@ export default function AboutMeSection() {
       <h2 className="relative mb-3 scroll-m-20 text-2xl font-semibold tracking-tight">
         About me
       </h2>
-      <div className="mb-4 max-w-[66ch] text-pretty text-[1.1rem] text-muted-foreground">
-        <p className="leading-7 not-first:mt-3">
+      <div className="text-muted-foreground mb-4 max-w-[66ch] text-[1.1rem] text-pretty">
+        {/* <p className="leading-7 not-first:mt-3">
           Hey! I&apos;m Facundo, a frontend developer with{" "}
           <span className="font-semibold">two years of experience</span>{" "}
           crafting web applications.
-        </p>
+        </p> */}
         <p className="leading-7 not-first:mt-3">
           I enjoy learning new things, solving problems, and paying attention to
           the details that make a difference. There&apos;s always room to grow,
           and I&apos;m always looking for ways to improve my skills and deliver
           meaningful solutions.
         </p>
-        <p className="leading-7  not-first:mt-3">
-          I&apos;d love to connect and work on something great together!
-        </p>
       </div>
       <div className="flex justify-start gap-2">
         <Button
           variant="default"
-          className="relative overflow-hidden rounded px-5 py-2.5 transition-all duration-300 hover:ring-2 hover:ring-primary/90 hover:ring-offset-2"
+          className="hover:ring-primary/90 relative overflow-hidden rounded px-5 py-2.5 transition-all duration-300 hover:ring-2 hover:ring-offset-2"
           asChild
         >
           <Link href={siteConfig.links.linkedin}>
@@ -40,7 +37,7 @@ export default function AboutMeSection() {
 
         <Button
           variant="secondary"
-          className="relative overflow-hidden rounded px-5 py-2.5 transition-all duration-300 hover:ring-2 hover:ring-primary/90 hover:ring-offset-2"
+          className="hover:ring-primary/90 relative overflow-hidden rounded px-5 py-2.5 transition-all duration-300 hover:ring-2 hover:ring-offset-2"
           asChild
         >
           <Link href={siteConfig.links.github}>
